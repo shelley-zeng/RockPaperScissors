575be160886b45e99f5a8b011536dc63461048e2
shelley-zeng <84942896+shelley-zeng@users.noreply.github.com>
2021-07-28 16:31:23 +1200

    Update RPS_V1.py

--- RPS_V1.py
+++ RPS_V1.py
@@ -1,33 +1,38 @@
 """
-Name:       Rock Paper Scissors
-Purpose:    This is a game of rock paper scissors
+Name:       General Knowledge Quiz
+Purpose:    This is a quiz on general knowledge
 Author:     Shelley Zeng
-Created:     21.05.2021
+Created:     30.06.2021
 """
-import random
-# List of computer moves
-COMPUTER_MOVES = ["Rock", "Paper", "Scissors"]
 
-# Quick little intro of game and what to type to play certain moves
-print("This is a game of rock paper scissors.")
-print("If you want to play rock type 'R'. \nIf you want to play paper type 'P'. \nIf you want to play scissors type 'S'")
-print("Rock beats scissors. Scissors beats paper. Paper beats rock.")
+#List on statements 
+STATEMENT_LIST = [
+    "The colours on Germany's flag is black, red and yellow.",
+    "Morocco is in North Africa.",
+    "James Madison was the 3rd American President.",
+    "Poseidon is the Greek god of the sea",
+    "1 Direction dispanded in 2015",
+    "the area of a triangle is A = bh",
+    "The Nile river is the longest river on Earth.",
+    "The Industrial Revolution started in the 1800's.",
+    "NASA was founded in 1958.",
+    "Snow White and The Seven Dwarfs was the first fully animated featured film to be released by Disney.",
+    "The Storm on the Sea of Galilee was painted by Leonardo da Vinci."
+]
 
-# Ask user to type chosen move
-user_rps_move = input("Type your choice: ")
+#List for if statement is true or false
+ANSWERS_LIST = ["t", "t", "f", "t", "f", "f", "t", "f", "t", "t", "f"] 
+#This is a welcome message for the user when they first go on the game so they know what to do
+print("""Hello! This is a true or false quiz on General Knowledge.\n 
+There will be 10 randomly generated questions. \n
+If you think the statement is true type "t".\n
+If you think the statement is false type "f".\n
+Let's begin! Good luck and have fun :)
+""")
 
-# Randomises a choice from the list of computer moves and prints move
-computer_move = random.choice(COMPUTER_MOVES)
-print(computer_move)
+#prints statements from the statement list.
+#not random and is numbered from 1-11\
+# first trial
 
-# If user and program type same move it is a tie
-if user_rps_move == "r" and computer_move == "Rock" or user_rps_move == "p" and computer_move == "Paper" or user_rps_move == "s" and computer_move == "Scissors":
-    print("tie")
-
-# If program uses a move that beats user print "i win!"
-elif user_rps_move == "r" and computer_move == "Paper" or user_rps_move == "p" and computer_move == "Scissors" or user_rps_move == "s" and computer_move == "Scissors":
-    print ("I win! ")
-
-# If user uses a move that beats program print "Good game! You win"
-else:
-    print("Good game! You win.")
+for index in range (len(STATEMENT_LIST)):
+    print("{})\n{}".format(index+1, STATEMENT_LIST[index]))
